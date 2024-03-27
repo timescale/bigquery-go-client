8d6a7abdaf1d5211c623452fcb29d76fefbd0a19
Nathaniel J Cochran <nathan@popsql.com>
2024-03-27 09:46:03 -0400

    Fix comment

--- option.go
+++ option.go
@@ -15,7 +15,7 @@ type jobOpt struct {
 // GetJob can be passed as an argument to a Query or Exec method to get a
 // handle on the BigQuery job for the query (e.g. to get its statistics after
 // the query completes). The *Job parameter must be non-nil. It will be
-// populated with a Job value by the time Query/Exec method returns.
+// populated with a Job value by the time the Query/Exec method returns.
 func GetJob(job *Job) jobOpt {
 	if job == nil {
 		panic(errors.New("GetJob expects non-nil *Job parameter"))
